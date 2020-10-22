20d9e99c695717dc6e1a642189f8cf115a691b29
Christopher Harrison <chrhar@microsoft.com>
2020-10-22 10:51:56 -0700

    Added database

--- api/dogs/index.ts
+++ api/dogs/index.ts
@@ -1,17 +1,35 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
+import * as mongoose from 'mongoose';
+mongoose.connect(process.env.MONGOOSE_CONNECTION_STRING, {useNewUrlParser: true});
+
 import { Dog } from '../../shared/interfaces.d';
+
+// MongoDB schema
+const dogSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+// Local document data type
+interface DogDocument extends Dog, mongoose.Document {}
+
+// Interface between my code and the database
+const DogModel = mongoose.model<DogDocument>('Dog', dogSchema);
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('Returned list of dogs');
 
-    const dogs: Array<Dog> = [
-        { name: 'Azure', age: 7 },
-        { name: 'Sammy', age: 2 },
-        { name: 'Dyson', age: 6 },
-        { name: 'Roscoe', age: 15 },
-        { name: 'Pookie', age: 8 },
-    ];
+    // temporary code to add dogs
+    // await DogModel.insertMany(
+    //     [
+    //         {name: 'Mongoose', age: 10},
+    //         {name: 'Sammy', age: 2}
+    //     ]
+    // );
+
+    // load all dogs
+    const dogs = await DogModel.find();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
